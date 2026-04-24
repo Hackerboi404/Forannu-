cc2355694beb006b06f9ff361fe0b41b623c4ec3
Hackerboi404 <tuturahul14rk@gmail.com>
2026-04-24 18:19:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import threading
-import asyncio
 from flask import Flask
 from bot import start_bot
 
@@ -10,16 +9,12 @@ app = Flask(__name__)
 def home():
     return "Bot is Alive! 🤖"
 
-def run_bot():
-    asyncio.run(start_bot())
-
 if __name__ == "__main__":
     print("🚀 Starting Server + Bot...")
 
-    # Bot thread me run hoga
-    t = threading.Thread(target=run_bot)
+    # Bot ko thread me direct run karo
+    t = threading.Thread(target=start_bot)
     t.start()
 
-    # Render port (IMPORTANT)
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
